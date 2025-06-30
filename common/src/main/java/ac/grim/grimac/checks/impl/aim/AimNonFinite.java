6b70998e8f08cb3eec9c4859b716a2bd43ed8619
Cr1spych <artem2040metra@gmail.com>
2025-06-30 16:04:51 +0300

    upd

--- common/src/main/java/ac/grim/grimac/checks/impl/aim/AimNonFinite.java
+++ common/src/main/java/ac/grim/grimac/checks/impl/aim/AimNonFinite.java
@@ -1,10 +1,12 @@
 package ac.grim.grimac.checks.impl.aim;
 
 import ac.grim.grimac.checks.Check;
+import ac.grim.grimac.checks.CheckData;
 import ac.grim.grimac.checks.type.RotationCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import ac.grim.grimac.utils.anticheat.update.RotationUpdate;
 
+@CheckData(name = "AimNonFinite")
 public class AimNonFinite extends Check implements RotationCheck {
     public AimNonFinite(GrimPlayer player) {
         super(player);
